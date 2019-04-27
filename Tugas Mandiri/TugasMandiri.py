ca8852af63481711186a7d5f8a2d5c18724c58ad
alham <alhamfebian68@gmail.com>
2019-04-27 19:43:09 +0700

    Tugas Mandiri

--- Tugas Mandiri/TugasMandiri.py
+++ Tugas Mandiri/TugasMandiri.py
@@ -5,30 +5,33 @@ import os
 
 # Mengambil nilai username dan password SSO UI dari environtment
 # Variabel di sistem operasi yang sedang berjalan. Jika environtmen variabel tidak ditemukan atau kosong, maka Python aka mengisi dengan string default
-USERNAME = os.getenv('UI_SSO_USERNAME', 'alham.febian')
-PASSWORD = os.getenv('UI_SSO_PASSWORD', 'alhamFbn25')
+USERNAME = os.getenv('UI_SSO_USERNAME', 'alhamfebian68')
+PASSWORD = os.getenv('UI_SSO_PASSWORD', 'Alhamfbn25')
 
 if __name__ == '__main__':
   # Membuat driver/'jembatan' ke Firefox
   driver = webdriver.Firefox()
-  driver.get('https://scele.cs.ui.ac.id')
+  driver.get('https://www.gmail.com/')
 
   # Mencari tag HTML yang digunakan untuk memasukkan username, lalu nilainya akan diisi dengan string dari variable USERNAME
-  username_input = driver.find_element_by_id('inputName')
+  username_input = driver.find_element_by_name('identifier')
   username_input.send_keys(USERNAME)
+  
+  form_username_next = driver.find_element_by_id('identifierNext')
+  form_username_next.click()
+  time.sleep(5)
 
   # Serupa dengan isian username di atas tapi untuk password
-  password_input = driver.find_element_by_id('inputPassword')
+  password_input = driver.find_element_by_name('password')
   password_input.send_keys(PASSWORD)
 
   # Melakukan submit form
-  login_form = driver.find_element_by_id('submit')
-  login_form.submit()
+  login_form = driver.find_element_by_id('passwordNext')
+  login_form.click()
+  time.sleep(5)
 
   # Tunggu paling lama 5 detik hingga browser mengembalikan halaman baru
   driver.implicity_wait(5)
-
-  find_EAI_lecture = driver.find_element_by_link_text('[SI.Reg] Enterprise Application Integration').click()
 
   try:
     # Jika gagal login, cetak pesan error dari halaman web ke shell
